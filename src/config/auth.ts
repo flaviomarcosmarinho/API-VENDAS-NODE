db2415680bc389582b6802b099024e17c826c8c2
Flavio <flaviomarcosmarinho@gmail.com>
2021-09-02 21:40:16 -0300

    Creating authentication middleware

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '74d2fe3480f9219f5fe701d601bd2509',
+    expiresIn: '1d',
+  },
+};
---
+++ src/modules/users/middlewares/isAutenticated.ts
@@ -0,0 +1,27 @@
+import auth from '@config/auth';
+import AppError from '@shared/errors/AppError';
+import { NextFunction, Request, Response } from 'express';
+import { verify } from 'jsonwebtoken';
+
+export default function isAutenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): void {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError('JWT Token is missing.');
+  }
+
+  // Bearer sdfjhskdf56465df6sd5v165s6d5f6s54ds1d2
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decodeToken = verify(token, auth.jwt.secret);
+
+    return next();
+  } catch {
+    throw new AppError('Invalid JWT Token.');
+  }
+}
